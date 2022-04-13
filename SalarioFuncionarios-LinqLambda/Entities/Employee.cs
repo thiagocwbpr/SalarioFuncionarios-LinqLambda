980c05512e569fc4617f74c5b363e91b7beb5dcb
Thiago Menezes <thiagocwbpr@gmail.com>
2022-04-13 16:47:26 -0300

    Finalizado o sistema, no qual vai retornar quais funcionários recebem um salário igual ou maior que o que foi digitado pelo utilizador. O sistema está utilizando expressão lambda, porém poderá ser adicionado expressão de SQL para consulta e retorno de valores do banco de dados (estou usando um arquivo de texto como banco de dados). Todos os dados de entrada são tratados, separados, e organizados em uma lista, no qual retorna na tela ao usuário, Nome, email, e salário do funcionário, e o valor total de salário, referente a esses funcionários.

--- SalarioFuncionarios-LinqLambda/Entities/Employee.cs
+++ SalarioFuncionarios-LinqLambda/Entities/Employee.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace SalarioFuncionarios_LinqLambda.Entities {
     internal class Employee {
-
         public string Name { get; set; }
         public string Email { get; set; }
         public double Salary { get; set; }
-
+        public Employee(string Name, string Email, double Salary) {
+            this.Name = Name;
+            this.Email = Email;
+            this.Salary = Salary;
+        }
+        public override string ToString() {
+            return Name + "\t " + Email + "\t - R$ " + Salary.ToString();
+        }
     }
 }
--- SalarioFuncionarios-LinqLambda/Program.cs
+++ SalarioFuncionarios-LinqLambda/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using SalarioFuncionarios_LinqLambda.Entities;
+using System.Linq;
+using System.Globalization;
 
 namespace SalarioFuncionarios_LinqLambda {
     internal class Program {
@@ -7,14 +11,31 @@ namespace SalarioFuncionarios_LinqLambda {
 
             string path = @"C:\Apps C#\Employees.txt";
 
+            List<Employee> employees = new List<Employee>();
+
+            Console.Write("Enter Salary - R$: ");
+            double SalaryInfo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("");
+
             using (StreamReader sr = File.OpenText(path)) {
                 while (!sr.EndOfStream) {
                     string[] line = sr.ReadLine().Split(',');
                     string name = line[0];
                     string email = line[1];
-                    double salary = double.Parse(line[2]);
+                    double salary = double.Parse(line[2], CultureInfo.InvariantCulture);
+                    employees.Add(new Employee(name, email, salary));
                 }
             }
+            var SalaryConsult = employees.Where(p => p.Salary >= SalaryInfo).Select(p => new { Nome = p.Name, p.Email, p.Salary }).DefaultIfEmpty();
+
+            Console.WriteLine("Name and Email whose salary is 'more' than R$: 2000.00\n");
+
+            foreach (var x in SalaryConsult) {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("");
+            var SumSalary = employees.Where(p => p.Salary >= SalaryInfo).Sum(p => p.Salary);
+            Console.WriteLine("Sum of salary of people in the search: R$: " + SumSalary.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
