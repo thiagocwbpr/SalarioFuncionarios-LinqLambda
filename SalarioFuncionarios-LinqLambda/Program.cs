dadc108e3f93f059b9defc2877440bd5d9795991
Thiago Menezes <thiagocwbpr@gmail.com>
2022-04-13 11:39:24 -0300

    Add project files.

---
+++ SalarioFuncionarios-LinqLambda/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SalarioFuncionarios_LinqLambda {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
