9aee8067b5b538adf0ffd07762159cdda60ee42d
Thiago Menezes <thiagocwbpr@gmail.com>
2022-04-13 13:54:20 -0300

    Criado a camada de entidades, e a classe Employee. Criado a lógica para a divisão de informações da fonte de dados que está sendo coletados os dados dos funcionários.

---
+++ SalarioFuncionarios-LinqLambda/Entities/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalarioFuncionarios_LinqLambda.Entities {
+    internal class Employee {
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+    }
+}
--- SalarioFuncionarios-LinqLambda/Program.cs
+++ SalarioFuncionarios-LinqLambda/Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.IO;
 
 namespace SalarioFuncionarios_LinqLambda {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            string path = @"C:\Apps C#\Employees.txt";
+
+            using (StreamReader sr = File.OpenText(path)) {
+                while (!sr.EndOfStream) {
+                    string[] line = sr.ReadLine().Split(',');
+                    string name = line[0];
+                    string email = line[1];
+                    double salary = double.Parse(line[2]);
+                }
+            }
         }
     }
 }
